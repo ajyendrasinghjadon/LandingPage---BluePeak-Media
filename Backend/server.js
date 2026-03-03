4f16a7c2b671e78ce9ec8602d213e16c4e985b79
ajyendrasinghjadon <ajyendrasinghjadonnn@gmail.com>
2026-03-03 16:44:04 +0530

    cors

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,10 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: ["https://bluepeakk-media.vercel.app/"],
+    credentials: true
+}));
 
 // Configure Nodemailer transporter
 const transporter = nodemailer.createTransport({
